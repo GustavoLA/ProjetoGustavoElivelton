1a5f2ef95c83f47332b2b415e231ac3281a5f805
gustavo_lourenco <gustavo_lourenco@10.1.48.219>
2013-09-24 14:00:48 -0300

    teste

--- src/br/com/view/Principal.java
+++ src/br/com/view/Principal.java
@@ -9,5 +9,10 @@ package br.com.view;
  * @author gustavo_lourenco
  */
 public class Principal {
-    
+
+    public static void main(String[] args) {
+        JanelaLogin jl = new JanelaLogin();
+        jl.setVisible(true);
+    }
 }
+
